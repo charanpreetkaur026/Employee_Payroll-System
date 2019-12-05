fe100d69f232179dceaf85fdd5f744d4afb79b6a
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 17:02:22 -0500

    [charan]commissionbasedPartTime class

--- app/src/main/java/com/example/employeepayrollsystem/models/CommissionbasedPartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/CommissionbasedPartTime.java
@@ -1,7 +1,15 @@
 package com.example.employeepayrollsystem.models;
 
 public class CommissionbasedPartTime extends  PartTime {
-    public CommissionbasedPartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hours) {
+
+    private float commission;
+
+    public float getCommission() { return commission; }
+
+    public void setCommission(int commission) { this.commission = commission; }
+
+    public CommissionbasedPartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hours, float commission) {
         super(id, name, age, earnings, birthYear, rate, hours);
+        this.commission = commission;
     }
 }
--- app/src/main/java/com/example/employeepayrollsystem/models/PartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/PartTime.java
@@ -3,16 +3,18 @@ package com.example.employeepayrollsystem.models;
 public class PartTime extends Employee {
 
     private int rate;
-    private float hours;
-    public PartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hours) {
+    private float hoursWorked;
+    public PartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hoursWorked) {
         super(id, name, age, earnings, birthYear);
+        this.rate = rate;
+        this.hoursWorked = hoursWorked;
     }
 
     public int getRate() { return rate; }
 
     public void setRate(int rate) { this.rate = rate; }
 
-    public float getHours() { return hours; }
+    public float getHours() { return hoursWorked; }
 
-    public void setHours(float hours) { this.hours = hours; }
+    public void setHours(float hours) { this.hoursWorked = hours; }
 }
