f27a2838604c51de2ae4a3d8d2c3eaed33aac478
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 17:05:44 -0500

    [charan]FixedBasedPartTime class constructor

--- app/src/main/java/com/example/employeepayrollsystem/models/FixedBasedPartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/FixedBasedPartTime.java
@@ -1,4 +1,7 @@
 package com.example.employeepayrollsystem.models;
 
-public class FixedBasedPartTime {
+public class FixedBasedPartTime extends PartTime {
+    public FixedBasedPartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hoursWorked) {
+        super(id, name, age, earnings, birthYear, rate, hoursWorked);
+    }
 }
