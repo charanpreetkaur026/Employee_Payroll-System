509c92e829917c240246f7da89b935cf415b9385
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 17:07:41 -0500

    [charan]FixedBasedPartTime class getter setter

--- app/src/main/java/com/example/employeepayrollsystem/models/FixedBasedPartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/FixedBasedPartTime.java
@@ -1,7 +1,15 @@
 package com.example.employeepayrollsystem.models;
 
 public class FixedBasedPartTime extends PartTime {
-    public FixedBasedPartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hoursWorked) {
+
+    private float fixedAmount;
+    public FixedBasedPartTime(int id, String name, int age, double earnings, int birthYear,
+                              int rate, float hoursWorked, float fixedamount) {
         super(id, name, age, earnings, birthYear, rate, hoursWorked);
+        this.fixedAmount = fixedamount;
     }
+
+    public float getFixedAmount() { return fixedAmount; }
+
+    public void setFixedAmount(float fixedAmount) { this.fixedAmount = fixedAmount; }
 }
