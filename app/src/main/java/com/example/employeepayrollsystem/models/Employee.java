0a13595e90693768d4c692b24685c94ba46c982b
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:52:49 -0500

    [charan] employee class -> getter, setter and constructor

--- app/src/main/java/com/example/employeepayrollsystem/models/Employee.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Employee.java
@@ -6,4 +6,46 @@ public abstract class Employee {
     private int age;
     double earnings;
     private int birthYear = 0 ;
+
+    public Employee(int id, String name, int age, double earnings, int birthYear) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.earnings = earnings;
+        this.birthYear = birthYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) { this.age = age; }
+
+    public double getEarnings() { return earnings; }
+
+    public void setEarnings(double earnings) { this.earnings = earnings; }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
 }
