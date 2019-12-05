be070c98f5ead2abde0549a86919dafdc05a29dd
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 17:18:22 -0500

    [charan] singleton class

---
+++ app/src/main/java/com/example/employeepayrollsystem/models/Singleton.java
@@ -0,0 +1,25 @@
+package com.example.employeepayrollsystem.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Singleton {
+    static Singleton singletonObj;
+    List<Employee> employees;
+    private Singleton(){
+        employees = new ArrayList<>();
+        //create objects of employee classes here
+
+
+    }
+    //to add employee objects to list
+    public void addtoList(Employee emp)
+    {
+        employees.add(emp);
+
+    }
+    public ArrayList<Employee> getList()
+    {
+        return  ((ArrayList<Employee>)this.employees);
+    }
+}
