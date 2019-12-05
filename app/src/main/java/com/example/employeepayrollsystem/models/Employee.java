3b92eaed7dcebcbfc46ae353e631df92489b3d8e
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:51:21 -0500

    [charan] employee class -> variables declared

--- app/src/main/java/com/example/employeepayrollsystem/models/Employee.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Employee.java
@@ -1,4 +1,9 @@
 package com.example.employeepayrollsystem.models;
 
-public class Employee {
+public abstract class Employee {
+    private int id;
+    private String name = "";
+    private int age;
+    double earnings;
+    private int birthYear = 0 ;
 }
