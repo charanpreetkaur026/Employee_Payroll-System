4cb58f3ca7225e2cab860c53a88d68b2cd0b339c
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:46:00 -0500

    [charan] viewModel class of contactUs Frgament deleted

--- app/src/main/java/com/example/employeepayrollsystem/ui/Employees/EmployeesFragment.java
+++ app/src/main/java/com/example/employeepayrollsystem/ui/Employees/EmployeesFragment.java
@@ -7,17 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.employeepayrollsystem.R;
-import com.example.employeepayrollsystem.ui.ContactUs.SendViewModel;
-
-import java.util.ArrayList;
 
 public class EmployeesFragment extends Fragment {
 
