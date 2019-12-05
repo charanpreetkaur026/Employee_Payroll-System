a81af14e33990bd78b2f9269bfd2a49db00b208e
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:55:43 -0500

    [charan] part time class extends employee

--- app/src/main/java/com/example/employeepayrollsystem/models/PartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/PartTime.java
@@ -1,5 +1,8 @@
 package com.example.employeepayrollsystem.models;
 
-public class PartTime {
+public class PartTime extends Employee {
 
+    public PartTime(int id, String name, int age, double earnings, int birthYear) {
+        super(id, name, age, earnings, birthYear);
+    }
 }
