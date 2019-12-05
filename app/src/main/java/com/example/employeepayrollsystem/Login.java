719c2768a951dbbba17a08bbb92ecc3eb8a94a3b
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 15:02:41 -0500

    [charan]  login activity login

--- app/src/main/java/com/example/employeepayrollsystem/Login.java
+++ app/src/main/java/com/example/employeepayrollsystem/Login.java
@@ -3,6 +3,7 @@ package com.example.employeepayrollsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,6 +16,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtEmail =findViewById(R.id.edtEmail);
-        edtPassword =findViewById()
+        edtPassword =findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+    }
+    public void buttonClick(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if()
+            }
+        });
     }
 }
