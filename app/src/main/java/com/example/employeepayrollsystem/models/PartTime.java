dfbaa457aee39842ba0278afe03c716ba7b7ffce
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:56:49 -0500

    [charan] part time class getter and setters

--- app/src/main/java/com/example/employeepayrollsystem/models/PartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/PartTime.java
@@ -2,7 +2,17 @@ package com.example.employeepayrollsystem.models;
 
 public class PartTime extends Employee {
 
+    private int rate;
+    private float hours;
     public PartTime(int id, String name, int age, double earnings, int birthYear) {
         super(id, name, age, earnings, birthYear);
     }
+
+    public int getRate() { return rate; }
+
+    public void setRate(int rate) { this.rate = rate; }
+
+    public float getHours() { return hours; }
+
+    public void setHours(float hours) { this.hours = hours; }
 }
