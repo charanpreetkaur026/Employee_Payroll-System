4632e41998454bd2bd5a41bccd487c742c95e033
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:59:34 -0500

    [charan]commissionbasedPartTime class extends Part Time

--- app/src/main/java/com/example/employeepayrollsystem/models/CommissionbasedPartTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/CommissionbasedPartTime.java
@@ -1,4 +1,7 @@
 package com.example.employeepayrollsystem.models;
 
-public class CommissionbasedPartTime {
+public class CommissionbasedPartTime extends  PartTime {
+    public CommissionbasedPartTime(int id, String name, int age, double earnings, int birthYear, int rate, float hours) {
+        super(id, name, age, earnings, birthYear, rate, hours);
+    }
 }
