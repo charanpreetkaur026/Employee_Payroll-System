819ddc34f239d4dde53d647a792f1328347cb779
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 16:40:58 -0500

    [charan] added recycler view to employees fragment

--- app/src/main/java/com/example/employeepayrollsystem/ui/Employees/EmployeesFragment.java
+++ app/src/main/java/com/example/employeepayrollsystem/ui/Employees/EmployeesFragment.java
@@ -1,5 +1,6 @@
 package com.example.employeepayrollsystem.ui.Employees;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,25 +12,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.employeepayrollsystem.R;
+import com.example.employeepayrollsystem.ui.ContactUs.SendViewModel;
+
+import java.util.ArrayList;
 
 public class EmployeesFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    Context con;
+    RecyclerView recyclerView;
+
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_employees, container, false);
         final TextView textView = root.findViewById(R.id.text_employees);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
