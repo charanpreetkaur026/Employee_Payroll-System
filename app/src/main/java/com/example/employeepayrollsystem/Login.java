a595f053dcc6df0cca26021dcb02db21363bdb5c
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 14:54:25 -0500

    [charan]  login activity finding ids

--- app/src/main/java/com/example/employeepayrollsystem/Login.java
+++ app/src/main/java/com/example/employeepayrollsystem/Login.java
@@ -3,12 +3,18 @@ package com.example.employeepayrollsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtEmail =findViewById(R.id.edtEmail);
+        edtPassword =findViewById()
     }
 }
