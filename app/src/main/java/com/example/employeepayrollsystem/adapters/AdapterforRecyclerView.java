f991153011768a690b8ba49407a9c18971749bd6
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 18:16:37 -0500

    [charan] created card layout for displaying employee list

---
+++ app/src/main/java/com/example/employeepayrollsystem/adapters/AdapterforRecyclerView.java
@@ -0,0 +1,96 @@
+package com.example.employeepayrollsystem.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.employeepayrollsystem.R;
+import com.example.employeepayrollsystem.models.Employee;
+
+import java.util.ArrayList;
+
+public class AdapterforRecyclerView extends RecyclerView.Adapter<AdapterforRecyclerView.ViewHolder> {
+
+
+
+
+    public interface SetCustomClickListener{
+        void customOnClick(Employee e);
+    }
+
+
+
+    ArrayList<Employee> arrayList;
+    Context context;
+    SetCustomClickListener listener;
+
+
+    public AdapterforRecyclerView(ArrayList<Employee> arrayList, Context context, SetCustomClickListener listener) {
+        this.arrayList = arrayList;
+        this.context = context;
+        this.listener = listener;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        View view = inflater.inflate(R.layout.layout_employee_cell, viewGroup , false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.bind(this.arrayList.get(i), this.listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.arrayList.size();
+    }
+
+    class ViewHolder extends  RecyclerView.ViewHolder{
+
+        TextView name;
+        //TextView gender;
+        TextView age;
+
+
+        public ViewHolder(@NonNull View View) {
+            super(View);
+            name = itemView.findViewById(R.id.emp_name);
+            //gender = itemView.findViewById(R.id.emp_gender);
+            age = itemView.findViewById(R.id.emp_age);
+        }
+
+
+//        public  void bind(final Employee e , final SetCustomClickListener listener)
+//        {
+//            if(e.getGen() == Gender.MALE )
+//            {
+//                gender.setText("MALE");
+//            }
+//            else
+//            {
+//                gender.setText("FEMALE");
+//            }
+//
+//            name.setText("Name : "+e.getName());
+//            age.setText("Age : "+e.getAge());
+//
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    listener.customOnClick(e);
+//                }
+//            });
+//        }
+    }
+}
