91932fe6f0593a62ac8ba5d067b003770a163010
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 15:50:10 -0500

    init method in car class

--- app/src/main/java/com/example/employeepayrollsystem/models/Car.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Car.java
@@ -4,5 +4,10 @@ public class Car extends Vehicle{
     private String carModel;
 
 
+    public Car(String make, String plate, String carModel) {
+        super(make, plate);
+        this.carModel =carModel;
+    }
 
+    
 }
