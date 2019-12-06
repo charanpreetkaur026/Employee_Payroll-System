30a59357a63d9ebcd36aeba47cd303f80baffb01
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 22:38:36 -0500

    [charan] created fragments-> partTime, FullTime, Intern

--- app/src/main/java/com/example/employeepayrollsystem/ui/add_employee/AddEmployeeFragment.java
+++ app/src/main/java/com/example/employeepayrollsystem/ui/add_employee/AddEmployeeFragment.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.employeepayrollsystem.R;
+import com.example.employeepayrollsystem.ui.Employees.FullTimeFragment;
+import com.example.employeepayrollsystem.ui.Employees.InternFragment;
+import com.example.employeepayrollsystem.ui.Employees.PartTimeFragment;
 
 public class AddEmployeeFragment extends Fragment implements View.OnClickListener{
 
@@ -25,6 +30,9 @@ public class AddEmployeeFragment extends Fragment implements View.OnClickListene
     RadioGroup gender;
     RadioGroup vehicle;
     RadioGroup employementtype;
+    PartTimeFragment partTimeFragment;
+    FullTimeFragment fullTimeFragment;
+    InternFragment internFragment;
 
 
     FragmentManager fragmentManager;
@@ -36,6 +44,59 @@ public class AddEmployeeFragment extends Fragment implements View.OnClickListene
         //final TextView textView = root.findViewById(R.id.text_add_employee);
         Log.e("TAG", "AddEmployeeFragment");
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.vehicle = view.findViewById(R.id.radio_group_vehicle);
+        this.text_age = view.findViewById(R.id.text_age);
+        this.text_name = view.findViewById(R.id.text_name);
+        this.text_date_of_birth = view.findViewById(R.id.text_date_of_birth);
+        this.employementtype = view.findViewById(R.id.radio_group_employment);
+        this.employementtype.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                FragmentTransaction fragmentTransaction;
+
+                switch (checkedId)
+                {
+                    case R.id.radio_parttime :
+                        if(AddEmployeeFragment.this.partTimeFragment  == null)
+                        {
+                            AddEmployeeFragment.this.partTimeFragment = new PartTimeFragment();
+
+                        }
+                        fragmentTransaction = AddEmployeeFragment.this.fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frame_layout_employment, AddEmployeeFragment.this.partTimeFragment);
+                        fragmentTransaction.commit();
+                        Toast.makeText(AddEmployeeFragment.this.getContext(), "parttime" , Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.radio_fulltime :
+                        if(AddEmployeeFragment.this.fullTimeFragment  == null)
+                        {
+                            AddEmployeeFragment.this.fullTimeFragment = new FullTimeFragment();
+                              }
+                        fragmentTransaction = AddEmployeeFragment.this.fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frame_layout_employment, AddEmployeeFragment.this.fullTimeFragment);
+                        fragmentTransaction.commit();
+                        Toast.makeText(AddEmployeeFragment.this.getContext(), "fulltime" , Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.radio_intern :
+                        if(AddEmployeeFragment.this.internFragment  == null)
+                        {
+                            AddEmployeeFragment.this.internFragment = new InternFragment();
+                             }
+                        fragmentTransaction = AddEmployeeFragment.this.fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.frame_layout_employment, AddEmployeeFragment.this.internFragment);
+                        fragmentTransaction.commit();
+                        Toast.makeText(AddEmployeeFragment.this.getContext(), "intern" , Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
+
     }
 
     @Override
