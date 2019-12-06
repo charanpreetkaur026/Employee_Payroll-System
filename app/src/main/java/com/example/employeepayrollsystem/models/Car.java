b0f435bca0684fa96ce25c713e249c190a7fa928
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 15:51:58 -0500

    changes

--- app/src/main/java/com/example/employeepayrollsystem/models/Car.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Car.java
@@ -1,19 +1,23 @@
 package com.example.employeepayrollsystem.models;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle
+{
     private String carModel;
 
 
-    public Car(String make, String plate, String carModel) {
+    public Car(String make, String plate, String carModel)
+    {
         super(make, plate);
         this.carModel =carModel;
     }
 
-    public String getCarModel() {
+    public String getCarModel()
+    {
         return carModel;
     }
 
-    public void setCarModel(String carModel) {
+    public void setCarModel(String carModel)
+    {
         this.carModel = carModel;
     }
 }
