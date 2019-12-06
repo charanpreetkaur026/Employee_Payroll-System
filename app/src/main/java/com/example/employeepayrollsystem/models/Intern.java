567e5b2402a67d8883cdf60e1a6527ce406f739a
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 18:24:53 -0500

    constructor of Intern class

--- app/src/main/java/com/example/employeepayrollsystem/models/Intern.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Intern.java
@@ -1,11 +1,17 @@
 package com.example.employeepayrollsystem.models;
 
-public class Intern
+public class Intern extends Employee
 {
     private String schoolname;
 
-    public Intern() {
-        super();
-    }
+//    public Intern() {
+//        super();
+//    }
 
+
+
+    public Intern(String schoolname, String name, int age , Vehicle vehicle ) {
+        super(name,age,vehicle);
+        this.schoolname = schoolname;
+    }
 }
