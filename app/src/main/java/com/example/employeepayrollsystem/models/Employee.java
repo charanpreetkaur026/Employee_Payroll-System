a5dd976abe713bc7837177e2230aad3cf850bc23
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 15:54:58 -0500

    employee class

--- app/src/main/java/com/example/employeepayrollsystem/models/Employee.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Employee.java
@@ -10,49 +10,5 @@ public abstract class Employee {
 
 
 
-    public Employee(int id, String name, int age, double earnings, int birthYear, Vehicle vehicle) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.earnings = earnings;
-        this.birthYear = birthYear;
-        this.vehicle = vehicle;
-    }
-    public Vehicle getVehicle() {
-        return vehicle;
-    }
-
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) { this.age = age; }
-
-    public double getEarnings() { return earnings; }
-
-    public void setEarnings(double earnings) { this.earnings = earnings; }
-
-    public int getBirthYear() { return birthYear; }
-
-    public void setBirthYear(int birthYear) { this.birthYear = birthYear; }
+    
 }
