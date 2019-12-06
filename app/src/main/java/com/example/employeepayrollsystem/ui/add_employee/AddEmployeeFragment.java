745e89aa88714d6597a5942e9959f6a55243b343
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-05 22:15:30 -0500

    [charan] addEmployee fragment -> fields declared

--- app/src/main/java/com/example/employeepayrollsystem/ui/add_employee/AddEmployeeFragment.java
+++ app/src/main/java/com/example/employeepayrollsystem/ui/add_employee/AddEmployeeFragment.java
@@ -5,11 +5,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -17,13 +19,21 @@ import com.example.employeepayrollsystem.R;
 
 public class AddEmployeeFragment extends Fragment implements View.OnClickListener{
 
+    TextView text_age;
+    TextView text_name;
+    TextView text_date_of_birth;
+    RadioGroup gender;
+    RadioGroup vehicle;
+    RadioGroup employementtype;
 
+
+    FragmentManager fragmentManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_add_employee, container, false);
-        final TextView textView = root.findViewById(R.id.text_add_employee);
+        //final TextView textView = root.findViewById(R.id.text_add_employee);
         Log.e("TAG", "AddEmployeeFragment");
         return root;
     }
