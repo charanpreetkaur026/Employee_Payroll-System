f9e975713f920fb37b38dbdd6357b8c4e3018eda
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 15:48:48 -0500

    car class

--- app/src/main/java/com/example/employeepayrollsystem/models/Car.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Car.java
@@ -4,16 +4,5 @@ public class Car extends Vehicle{
     private String carModel;
 
 
-    public Car(String make, String plate, String carModel) {
-        super(make, plate);
-        this.carModel =carModel;
-    }
 
-    public String getCarModel() {
-        return carModel;
-    }
-
-    public void setCarModel(String carModel) {
-        this.carModel = carModel;
-    }
 }
