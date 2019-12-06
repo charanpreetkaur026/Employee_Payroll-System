fa7404bc109a7a3cfcc47576edab7c2aa871b521
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 18:15:36 -0500

    get set in Full Time

--- app/src/main/java/com/example/employeepayrollsystem/models/FullTime.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/FullTime.java
@@ -11,4 +11,24 @@ public class FullTime extends Employee
         this.bonus = bonus;
     }
 
+    public int getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(int salary)
+    {
+        this.salary = salary;
+    }
+
+    public int getBonus()
+    {
+        return bonus;
+    }
+
+    public void setBonus(int bonus)
+    {
+        this.bonus = bonus;
+    }
+
 }
