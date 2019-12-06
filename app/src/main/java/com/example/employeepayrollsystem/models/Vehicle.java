5d7d959d90eeec414b88d30925d7dbf72126fb34
Komal2006 <komaldeepkr99@gmail.com>
2019-12-06 15:58:17 -0500

    minor changes in vehicle class

--- app/src/main/java/com/example/employeepayrollsystem/models/Vehicle.java
+++ app/src/main/java/com/example/employeepayrollsystem/models/Vehicle.java
@@ -1,13 +1,27 @@
 package com.example.employeepayrollsystem.models;
 
-public abstract class Vehicle {
+public abstract class Vehicle
+{
     private String make;
     private String plate;
 
-    public Vehicle(String make, String plate) {
+    public Vehicle(String make, String plate)
+    {
         this.make = make;
         this.plate = plate;
     }
 
-    
+    public String getMake() { return make; }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
 }
