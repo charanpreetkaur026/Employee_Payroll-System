a0c97ad74f1e7fa91525476a71ca145dd550041d
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-06 19:14:42 -0500

    [charan] minor changes

--- app/src/main/java/com/example/employeepayrollsystem/ui/employee_details/EmployeeDetailsFragment.java
+++ app/src/main/java/com/example/employeepayrollsystem/ui/employee_details/EmployeeDetailsFragment.java
@@ -139,7 +139,7 @@ public class EmployeeDetailsFragment extends Fragment implements DataFromEmploye
 
 
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+
         void onFragmentInteraction(Uri uri);
     }
 }
