fc600a579d07c06e91fea7c2ebbdb6d617843f47
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-12-11 11:47:55 -0500

    [charan] added drawable dollor to input fields

--- app/src/main/java/com/example/employeepayrollsystem/ui/help/HelpFragment.java
+++ app/src/main/java/com/example/employeepayrollsystem/ui/help/HelpFragment.java
@@ -20,22 +20,22 @@ public class HelpFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-       // View root = inflater.inflate(R.layout.fragment_help, container, false);
+       View root = inflater.inflate(R.layout.fragment_home, container, false);
         //View root = inflater.inflate(showAlert(), container,false);
         //final TextView textView = root.findViewById(R.id.text_about_us);
         Log.e("TAG", "HelpFragment");
         showAlert();
 
-        return null;
+        return root;
     }
     //com/example/fragmentexample/SecondActivity.java:69
     public void showAlert(){
         androidx.appcompat.app.AlertDialog.Builder alertDailogBuilder = new androidx.appcompat.app.AlertDialog.Builder(getActivity());
         // alertDailogBuilder.setIcon(R.drawable.);
         alertDailogBuilder.setTitle("Contact Us ");
-        alertDailogBuilder.setMessage("Drop an Email at: admin@payroll.com");
+        //alertDailogBuilder.setMessage("Drop an Email at: admin@payroll.com");
         alertDailogBuilder.setMessage("Drop an Email at: admin@payroll.com\n" +
-                "Comapny Direct: (+1)234-567-89999");
+                "Company Direct: (+1)234-567-89999");
         alertDailogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
